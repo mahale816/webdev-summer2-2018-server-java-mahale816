6ad04267f4893187313c6bb7d132e3cb71df763c
mahale816 <mahale.c@husky.neu.edu>
2018-08-01 14:09:58 -0400

    Widgets

---
+++ src/main/java/com/example/webdevsummer22018serverjavamahale816/repositories/WidgetRepository.java
@@ -0,0 +1,24 @@
+package com.example.webdevsummer22018serverjavamahale816.repositories;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.example.webdevsummer22018serverjavamahale816.models.Topic;
+import com.example.webdevsummer22018serverjavamahale816.models.Widget;
+
+public interface WidgetRepository extends CrudRepository<Widget, Integer>{
+	
+	@Transactional
+	@Modifying
+	@Query("DELETE FROM Widget w WHERE w.topic.id=:topicId")
+	void deleteWidgetsByTopicId(@Param("topicId") int topicId);
+	
+	@Query("SELECT w from Widget w WHERE w.topic=:topic ORDER BY w.id")
+	List<Widget> findAllWidgetsByTopicSorted(@Param("topic") Topic topic);
+}
